58d017dced8f68e74f6e4f075180b155e3eba357
natakozlova <natabalabanova555@yandex.ru>
2023-05-17 13:10:11 -0700

    initial commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,11 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import io.restassured.http.ContentType;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +19,8 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
     }
 }
